430a4ce9436d99c5e0a0d1aab7a742bda7dc9a10
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-08-02 13:08:45 +0330

    Configure the coresi/modd package for hot reloading.

--- 01_lenslocked/main.go
+++ 01_lenslocked/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome web site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my great web site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
-	fmt.Println("Starting the server on :3000")
+	fmt.Println(" 🚀 server is running on port :3000 ✅")
 	http.ListenAndServe(":3000", nil)
 }
