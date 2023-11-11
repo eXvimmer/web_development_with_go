1ed8f6301360eb85d18811829d84f6ff03b86f8f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-11 16:41:04 +0330

    Create a random packages, that uses the crypto/rand module, for generating session tokens.

---
+++ 01_lenslocked/rand/rand.go
@@ -0,0 +1,30 @@
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+)
+
+const sessionTokenBytes = 32
+
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, fmt.Errorf("rand/Bytes: %w", err)
+	}
+	return b, nil
+}
+
+func String(n int) (string, error) {
+	b, err := Bytes(n)
+	if err != nil {
+		return "", fmt.Errorf("rand/String: %w", err)
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+func SessionToken() (string, error) {
+	return String(sessionTokenBytes)
+}
