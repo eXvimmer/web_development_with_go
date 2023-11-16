fa27000019a054ec39da0ac22e370210148baea5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-16 22:00:51 +0330

    Convert to schema migrations.

--- 01_lenslocked/migrations/20231116215218_sessions.sql
+++ 01_lenslocked/migrations/20231116215218_sessions.sql
@@ -1,5 +1,13 @@
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS sessions (
   id SERIAL PRIMARY KEY,
   user_id INT UNIQUE NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   token_hash TEXT UNIQUE NOT NULL
 );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sessions;
+-- +goose StatementEnd
