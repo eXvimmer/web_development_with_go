0b98ff2a50841cc0ba3ca38de51fcecbdf8cd599
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-25 21:02:58 +0330

    Implement password reset creation.

--- 01_lenslocked/models/password_reset.go
+++ 01_lenslocked/models/password_reset.go
@@ -1,9 +1,14 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"strings"
 	"time"
+
+	"github.com/exvimmer/lenslocked/rand"
 )
 
 const (
@@ -35,12 +40,57 @@ type PasswordResetService struct {
 	Duration time.Duration
 }
 
-// TODO: implement this
 func (service *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: this function is not implemented")
+	email = strings.ToLower(email)
+	var userId int
+	row := service.DB.QueryRow(`
+		SELECT id FROM users
+		WHERE email = $1;
+	`, email)
+	err := row.Scan(&userId)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	bytesPerToken := service.BytesPerToken
+	if bytesPerToken < MinBytePerToken {
+		bytesPerToken = MinBytePerToken
+	}
+	token, err := rand.String(bytesPerToken)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	duration := service.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+	pwReset := PasswordReset{
+		UserId:    userId,
+		Token:     token,
+		TokenHash: service.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+	row = service.DB.QueryRow(`
+		INSERT INTO password_resets (user_id, token_hash, expires_at)
+		VALUES
+			($1, $2, $3)
+		ON CONFLICT (user_id) DO
+		UPDATE
+		SET token_hash = $2, expires_at = $3
+		RETURNING id;
+	`, pwReset.UserId, pwReset.TokenHash, pwReset.ExpiresAt)
+	err = row.Scan(&pwReset.Id)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+	return &pwReset, nil
 }
 
 // TODO: implement this
 func (service *PasswordResetService) Consume(token string) (*User, error) {
 	return nil, fmt.Errorf("TODO: this function is not implemented")
+}
+
+func (service *PasswordResetService) hash(token string) string {
+	sum := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(sum[:])
 }
