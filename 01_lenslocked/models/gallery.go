23f3407427ed5a8b079104b0348b870f17fa1ee2
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-29 16:26:45 +0330

    Add functionality to create galleries in database.

--- 01_lenslocked/models/gallery.go
+++ 01_lenslocked/models/gallery.go
@@ -1,7 +1,34 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Gallery struct {
 	Id     int
 	Title  string
 	UserId int
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (gs *GalleryService) Create(title string, userId int) (*Gallery, error) {
+	gallery := Gallery{
+		Title:  title,
+		UserId: userId,
+	}
+	// TODO: add validation
+	row := gs.DB.QueryRow(`
+		INSERT INTO galleries (title, user_id)
+		VALUES ($1, $2)
+		RETURNING id;
+	`, title, userId)
+	err := row.Scan(&gallery.Id)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+	return &gallery, nil
 }
